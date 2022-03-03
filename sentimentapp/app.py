c61118e0e2335c37c9c57aabfd45fabe22536967
hsdistefa <hsdistefa@gmail.com>
2022-03-03 15:53:42 -0800

    Added headline submission results page

--- sentimentapp/app.py
+++ sentimentapp/app.py
@@ -3,10 +3,14 @@ Given a Financial News headline, returns sentiment score based our trained
 model
 """
 import flask
+from flask import render_template, request, flash
 
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'A#IPmd83B*d7Z'
+
+LAST_HEADLINE = None
 
 # test data
 sentiment_scores = [
@@ -18,6 +22,14 @@ sentiment_scores = [
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return flask.render_template('home.html')
+    if request.method == 'POST':
+        headline = request.form['headline']
+    if not headline:
+        flash('Headline is required!')
+    else:
+        return render_template('home_action.html', headline=headline)
+
+    return render_template('home.html')
+
 
 app.run()
